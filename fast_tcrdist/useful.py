d8f27508af10fd3c954483256c5a3f3dfc6f4f62
Neal Smith <nealpsmith@bcb-comp.partners.org>
2020-08-29 14:31:29 -0400

    removed unnecessary code

--- fast_tcrdist/useful.py
+++ fast_tcrdist/useful.py
@@ -1,28 +1,3 @@
-# def random_colors (n_cols) :
-# 	import random
-# 	num = lambda: random.randint(0,255)
-
-# 	cols = set()
-# 	for i in range(n_cols) :
-# 		cols.add("#%02X%02X%02X" % (num(),num(),num()))
-# 	return(cols)
-
-# def create_color_dict (categories) :
-# 	colors = random_colors(len(categories))
-
-# 	color_dict = dict(zip(categories, colors))
-
-# 	return(color_dict)
-
-# def factor_colors(data, factor):
-	
-# 	# Get the unique genes
-# 	unique_factors = set(data.obs[factor])
-# 	cdict = create_color_dict(unique_factors)
-
-# 	data.uns["{factor}_colors".format(factor = factor)] = cdict
-
-
 # Took large color palette from scanpy: https://github.com/theislab/scanpy/blob/master/scanpy/plotting/palettes.py
 godsnot_102 = [
     # "#000000",  # remove the black, as often, we have black colored annotation
